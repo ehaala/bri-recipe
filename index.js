2207c9b9b0934b8a938dbcb00827d4b69e64d95c
ehaala <ejhaala@gmail.com>
2018-02-13 15:04:44 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var request = require('request');
+var ejsLayouts = require('express-ejs-layouts');
+var bodyParser = require('body-parser');
+var app = express();
+var path = require('path');
+var db = require('./models');
+
+var session = require('express-session');
+var flash = require('connect-flash');
+var isLoggedIn = require('./middleware/isLoggedIn');
+
+app.set('view engine', 'ejs');
+
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(ejsLayouts);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(flash());
+
+app.use(function(req, res, next) {
+  // before every route, attach the flash messages and current user to res.locals
+  res.locals.alerts = req.flash();
+  res.locals.currentUser = req.user;
+  next();
+});
+
+//these lines MUST occur after the session is configured
+var passport = require('./config/ppConfig');
+// initialize the passport configuration and session as middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.get('/profile', isLoggedIn, function(req, res) {
+	db.user.findOne({
+		where: {id: req.user.id}
+	}).then(function(user) {
+		res.render('profile', {user: user});
+	})
+});
+
+app.use('/auth', require('./controllers/auth'));
+
+var server = app.listen(process.env.PORT || 3000);
+
+module.exports = server;
